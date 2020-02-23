f1777b114b998d5ae2fcf5f5f491bb4529d2f7f5
Florence-Njeri <flonsie17@gmail.com>
2020-02-23 13:23:39 +0300

    written first test

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,33 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.junit.Assert.*
+import org.junit.Test
+
+class StatisticsUtilsTest{
+    //For 1 active task there should be 100% active tasks n 0% complete tasks
+    @Test
+    fun getActiveAndCompletedStats_noCompleted_returnsHundredZero(){
+        val task= listOf<Task>(Task("title","description",false))
+
+        val result= getActiveAndCompletedStats(task)
+        assertEquals(0f,result.completedTasksPercent)
+        assertEquals(100f,result.activeTasksPercent)
+    }
+
+
+    @Test
+    fun getActiveAndCompletedStats_both_returnsFourtySixty(){
+        val task= listOf<Task>(
+                Task("title","description",true),
+                Task("title","description",true),
+                Task("title","description",false),
+                Task("title","description",false),
+                Task("title","description",false)
+                        )
+
+        val result= getActiveAndCompletedStats(task)
+        assertEquals(40f,result.completedTasksPercent)
+        assertEquals(60f,result.activeTasksPercent)
+    }
+}
